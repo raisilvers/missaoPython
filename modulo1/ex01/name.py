37ad6a72e17b3e60f733827fe5db8deca6365231
Rai <raidasilva1992@gmail.com>
2025-12-14 17:23:33 -0300

    finalizado modulo1

---
+++ modulo1/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Raimundo"
+last_name = "Silva"
+whole_name = first_name +" "+ last_name
+
+print(whole_name)
---
+++ modulo1/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+last_name = input("Ei, qual o seu primeiro nome? ")
+first_name = input("E seu sobrenome? ")
+print(f"Bem, prazer em conhecê-lo, {last_name} {first_name}.")

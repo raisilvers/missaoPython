d3b9ba66a26514b8291657535185bfe53f690bbe
Rai <raidasilva1992@gmail.com>
2026-01-01 21:22:55 -0300

    finalizado logica Ex00

--- missaoPython/modulo3/ex00/to25.py
+++ missaoPython/modulo3/ex00/to25.py
@@ -2,7 +2,9 @@
 
 n = int(input("Enter a number less than 25: "))
 
-x = 26
-while n < x:
-    x = x - 1
-    print(x)
+if n > 25:
+    print("ERROR")
+else:
+    while n <= 25:
+        print(n)
+        n = n + 1
